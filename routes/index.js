084c159f1aa1719cc94dec5b8b25a9c513598675
malcolmseyd <malcolmseyd@gmail.com>
2019-11-23 14:56:42 -0800

    time

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/play', function(req, res, next) {
+router.post('/play', function(req, res, next) {
   //res.render('index', { title: 'PLAYING' });
   console.log('play request received')
   var data;
@@ -26,7 +26,7 @@ router.get('/play', function(req, res, next) {
   })
 })
 
-router.get('/pause', function(req, res, next) {
+router.post('/pause', function(req, res, next) {
   //res.render('index', { title: 'PAUSED' });
   console.log('pause request received')
   var data;
@@ -35,7 +35,9 @@ router.get('/pause', function(req, res, next) {
       throw err;
     }
     data = JSON.parse(data)
-    data.playing = false
+    data.playing = req.body.playbackState
+    data.currentTime = req.body.playbackTime
+    data.sliderHead = req.body.playbackTime
     data = JSON.stringify(data)
     fs.writeFile('public/data/appState.json', data, 'utf8', function(err) {
       if (err) return console.log(err);
@@ -44,17 +46,28 @@ router.get('/pause', function(req, res, next) {
 })
 
 router.post('/shiftSlider', function(req,res,next) {
-  req.body.value
   fs.readFile('public/data/appState.json', 'utf8', function(err, data) {
     if (err) {
       throw err;
     }
     data = JSON.parse(data)
-    data.sliderHead = req.body.value
+    data.sliderHead = req.body.playbackTime
+    data.currentTime = req.body.playbackTime
+    data.playing = true
     data = JSON.stringify(data)
     fs.writeFile('public/data/appState.json', data, 'utf8', function(err) {
       if (err) return console.log(err);
     })
+  })
+})
+
+router.get('/getState', function(req, res, next) {
+  fs.readFile('public/data/appState.json', 'utf8', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    data = JSON.parse(data)
+    res.send(data)
   })
 })
 
