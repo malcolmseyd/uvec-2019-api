c6f89d2dce8fbea474d164631978f5c750460a46
ryleyw <root@ND053694.idir.BCGOV>
2019-11-23 11:58:24 -0800

    testing for pause paly methods

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+router.get('/play', function(req, res, next) {
+  //res.render('index', { title: 'PLAYING' });
+  console.log('play request received')
+})
+
+router.get('/pause', function(req, res, next) {
+  //res.render('index', { title: 'PAUSED' });
+  console.log('pause request received')
+})
+
+router.post('/shiftSlider', function(req,res,next) {
+  console.log('shiftSlider received')
+})
+
+router.post('/connect', function (req, res, next) {
+  console.log('connect intialized')
+})
+
+router.post('/disconnect', function (req, res, next) {
+  console.log('disconnect insitialzed')
+})
+
+module.exports = router; 
